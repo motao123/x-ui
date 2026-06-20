90810b17106402f5ba51d8180c09fad0922ae569
小猪会飞 <motaoxx@outlook.com>
2026-06-20 11:02:54 +0800

    fix(acme): require email before certificate request

--- x-ui.sh
+++ x-ui.sh
@@ -409,7 +409,7 @@ show_enable_status() {
 
 check_xray_status() {
     count=$(ps -ef | grep "xray-linux" | grep -v "grep" | wc -l)
-    if [[ count -ne 0 ]]; then
+    if [[ ${count} -ne 0 ]]; then
         return 0
     else
         return 1
